359353369423f0e9346f3d6561475a6a3c432172
nandanisahu330 <nandanisahu1409@gmail.com>
2025-08-06 19:31:05 +0530

    Update MainClass.java
    record

--- MainClass.java
+++ MainClass.java
@@ -1,1 +1,9 @@
 
+package student_management;
+
+public class MainClass {
+	    public static void main(String[] args) {
+	        StudentManagement sm = new StudentManagement();
+	      sm.menu();
+	    }
+	}
