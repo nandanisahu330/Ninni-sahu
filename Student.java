e8ac94b53b08472aa843b36c4936e894e98899e5
nandanisahu330 <nandanisahu1409@gmail.com>
2025-08-06 19:34:12 +0530

    Update Student.java
    record

--- Student.java
+++ Student.java
@@ -1,1 +1,47 @@
+package student_management;
 
+public class Student {
+	public int id;
+	public String name;
+	public double marks;
+	
+	// arguemented constructor
+	
+	public Student(int id, String name, double marks) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+	}
+
+	//getters and setters
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+	//tostring method for usefulinformation
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + "]";
+	}
+}
